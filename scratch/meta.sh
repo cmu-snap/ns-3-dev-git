98e8fb2ceac721f448d2830526893c4789859f5e
Christopher Canel <c.canel@icloud.com>
2023-04-11 19:53:52 +0000

    Clean up meta.sh

--- scratch/meta.sh
+++ scratch/meta.sh
@@ -4,40 +4,58 @@
 
 set -eou pipefail
 
-out_dir="$1"
-dir_name="$2"
-rm -rfv "${out_dir:?}/${dir_name:?}"
-mkdir -p "$out_dir/$dir_name/"{log,pcap}
+# Enforce that there is a single argument.
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <output directory>"
+    exit 1
+fi
 
 burstDurationMs=15
-lineRateGbps=12.5
-numSenders=500
+numBursts=5
+numSenders=214 # $((5 + 1))
+cca="TcpDctcp"
+nicRateMbps=12500
+uplinkRateMbps=100000
+delayPerLinkUs=5
+jitterUs=100
 metaQueueSizeBytes=1800000
 metaQueueThresholdBytes=120000
 bytesPerPacket=1500
 queueSizePackets="$(python -c "import math; print(math.ceil($metaQueueSizeBytes / $bytesPerPacket))")"
 thresholdPackets="$(python -c "import math; print(math.ceil($metaQueueThresholdBytes / $bytesPerPacket))")"
 # Convert burst duration to bytes per sender.
-bytesPerSender="$(python -c "import math; print(math.ceil(($burstDurationMs / 1e3) * ($lineRateGbps * 1e9 / 8) / $numSenders))")"
+bytesPerSender="$(python -c "import math; print(math.ceil(($burstDurationMs / 1e3) * ($nicRateMbps * 1e6 / 8) / $numSenders))")"
+icwnd=10
+firstFlowOffsetMs=0 # 10
+
+out_dir="$1"
+dir_name="${burstDurationMs}ms-$numSenders-$numBursts-$cca-${icwnd}icwnd-${firstFlowOffsetMs}offset"
+results_dir="$out_dir/$dir_name"
+rm -rfv "${results_dir:?}"
+mkdir -p "$results_dir/"{log,pcap}
 
 ns3_dir="$(realpath "$(dirname "$0")/..")"
 
 "$ns3_dir/ns3" build "scratch/incast"
 
 "$ns3_dir"/build/scratch/ns3-dev-incast-default \
-    --outputDirectory="$out_dir" \
+    --outputDirectory="$out_dir/" \
     --traceDirectory="$dir_name" \
     --numSenders=$numSenders \
     --bytesPerSender="$bytesPerSender" \
-    --numBursts=3 \
-    --delayPerLinkUs=5 \
-    --jitterUs=20 \
-    --smallLinkBandwidthMbps=12500 \
-    --largeLinkBandwidthMbps=100000 \
-    --cca=TcpDctcp \
+    --numBursts=$numBursts \
+    --delayPerLinkUs=$delayPerLinkUs \
+    --jitterUs=$jitterUs \
+    --smallLinkBandwidthMbps=$nicRateMbps \
+    --largeLinkBandwidthMbps=$uplinkRateMbps \
+    --cca=$cca \
     --smallQueueSizePackets="$queueSizePackets" \
     --largeQueueSizePackets="$queueSizePackets" \
     --smallQueueMinThresholdPackets="$thresholdPackets" \
     --smallQueueMaxThresholdPackets="$thresholdPackets" \
     --largeQueueMinThresholdPackets="$thresholdPackets" \
-    --largeQueueMaxThresholdPackets="$thresholdPackets"
+    --largeQueueMaxThresholdPackets="$thresholdPackets" \
+    --initialCwnd=$icwnd \
+    --firstFlowOffsetMs=$firstFlowOffsetMs
+
+echo "Results in: $results_dir"
